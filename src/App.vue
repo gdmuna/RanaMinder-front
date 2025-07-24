db99f6a8ca416ad075e88c6fac8c675804fe92ea
FOV-RGT <Q1950088436@outlook.com>
2025-07-25 01:59:30 +0800

    初始化项目框架

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+import { CustomBounce } from "gsap/CustomBounce";
+import { SlowMo } from "gsap/EasePack";
+import { Draggable } from "gsap/Draggable";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { Flip } from "gsap/Flip";
+import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { Observer } from "gsap/Observer";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { SplitText } from "gsap/SplitText";
+import { TextPlugin } from "gsap/TextPlugin";
+
+import HelloWorld from './components/HelloWorld.vue'
+import { Toaster, toast } from 'vue-sonner'
+import 'vue-sonner/style.css'
+
+import { onBeforeMount, ref } from 'vue'
+
+
+onBeforeMount((): void => {
+  // Initialize GSAP plugins
+  gsap.registerPlugin(
+    CustomEase,
+    CustomBounce,
+    SlowMo,
+    Draggable,
+    DrawSVGPlugin,
+    Flip,
+    InertiaPlugin,
+    MotionPathHelper,
+    MotionPathPlugin,
+    MorphSVGPlugin,
+    Observer,
+    Physics2DPlugin,
+    PhysicsPropsPlugin,
+    ScrollTrigger,
+    ScrollToPlugin,
+    SplitText,
+    TextPlugin
+  );
+})
+
+
+</script>
+
+<template>
+  <div>
+    <Toaster />
+    <a href="https://vite.dev" target="_blank" class="flex justify-center">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank" class="flex justify-center">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
