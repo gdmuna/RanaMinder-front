c6643f18da5e6cf4863629d88dd73b486fe30aa0
xl-xlxl <xlxl0495@outlook.com>
2025-08-16 16:22:21 +0800

    暂存

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,11 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false, // css in js
-        }),
-      ],
-    }),
   ],
   resolve: {
     alias: {
